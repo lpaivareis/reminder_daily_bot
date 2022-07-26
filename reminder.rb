5345963a2ccf0a9bfba2cd25c2a10407e9e7b6c6
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-07-26 18:30:10 -0300

    fix cron

--- reminder.rb
+++ reminder.rb
@@ -6,7 +6,7 @@ scheduler = Rufus::Scheduler.new
 bot = Discordrb::Bot.new token: ENV["DISCORD_TOKEN"]
 channel_id = ENV["CHANNEL_ID"]
 
-scheduler.cron '* * * * *' do
+scheduler.cron '0 10 * * *' do
   bot.send_message(channel_id, 'O que você vai fazer hoje? @everyone')
   bot.run 
 end
