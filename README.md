d097082fe4dd01239eee5c238f07e5239f3b031b
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-07-26 18:27:45 -0300

    add bot of reminder of daily

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# reminder_daily_bot
+#### Reminder daily bot
+
+Bot para estudo da gem [discordrb](https://github.com/shardlab/discordrb).
---
+++ bot.rb
@@ -0,0 +1,14 @@
+require 'discordrb'
+require 'dotenv/load'
+require 'rufus-scheduler'
+
+scheduler = Rufus::Scheduler.new
+bot = Discordrb::Bot.new token: ENV["DISCORD_TOKEN"]
+channel_id = ENV["CHANNEL_ID"]
+
+scheduler.cron '* * * * *' do
+  bot.send_message(channel_id, 'O que você vai fazer hoje? @everyone')
+  bot.run 
+end
+
+scheduler.join
